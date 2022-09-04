785f6490f8e6a870c18e7f1cbb920529ade3cb0b
Aman Kumar <amanmodi104@gmail.com>
2022-09-04 15:20:07 +0530

    mathematical operators problems

---
+++ assignment-day-2/problem3.js
@@ -0,0 +1,12 @@
+
+let a = 10;
+let b = 11;
+if(a>b){
+  console.log(a, "is bigger than", b);
+}else if(a<b){
+console.log(b,"is bigger than",a);
+}else{
+  console.log("both equal");
+  
+  
+}
---
+++ assignment-day-2/problem4.js
@@ -0,0 +1,20 @@
+
+
+let user_name = ("amnmodi104@gmail.com");
+let password = ("987654321");
+
+let ent_userName = ("amnmodi104@gmail.com");
+let ent_password = ("987654321");
+
+if(ent_userName==user_name){
+  if(ent_password==password){
+    console.log("Login Succesfull")
+  }else{
+    console.log("incorrect password")
+  }
+}else{
+  console.log("wrong password");
+  
+  
+    
+  }  
